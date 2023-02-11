27075cb7adc69a92dfad950b5cd1770d9e0a00a3
haru0801 <maru0819@docomo.ne.jp>
2023-02-11 19:40:54 +0900

    seeder

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -17,42 +17,48 @@ class PostSeeder extends Seeder
     {
         DB::table('posts')->insert([
             [
-            'title' => 'top',
-            'body' => '着物',
+            'title' => '結婚式',
+            'body' => '夫婦で着物を着ました。',
             'user_id' => '1',
-            'image_url' =>'https://res.cloudinary.com/djyg6jqy2/image/upload/v1675156843/%E7%9D%80%E7%89%A9_do2toa.jpg'
+            'image_url' =>'https://res.cloudinary.com/djyg6jqy2/image/upload/v1676111459/%E3%82%B3%E3%83%BC%E3%83%87%EF%BC%95_lol9pu.jpg',
+            'created_at' => new DateTime(),
             ],
             
             [
-            'title' => 'ready',
-            'body' => 'スーツ',
+            'title' => '夏のコーデ',
+            'body' => 'シャツ',
             'user_id' => '1',
-            'image_url' =>'https://res.cloudinary.com/djyg6jqy2/image/upload/v1675156810/%E3%82%B9%E3%83%BC%E3%83%84_hkncqz.jpg'
+            'image_url' =>'https://res.cloudinary.com/djyg6jqy2/image/upload/v1676111432/%E3%82%B3%E3%83%BC%E3%83%87%EF%BC%96_us6c9y.jpg',
+            'created_at' => new DateTime(),
             ],
             
             [
-            'title' => 'mow',
-            'body' => 'ワンピース',
+            'title' => '今日のコーデ♪',
+            'body' => 'ニットベストを使ったコーデ',
             'user_id' => '4',
-            'image_url' =>'https://res.cloudinary.com/djyg6jqy2/image/upload/v1675156837/%E3%83%AF%E3%83%B3%E3%83%94%E3%83%BC%E3%82%B9_qxgvv4.jpg'
+            'image_url' =>'https://res.cloudinary.com/djyg6jqy2/image/upload/v1676111453/%E3%82%B3%E3%83%BC%E3%83%87%EF%BC%93_kchglo.jpg',
+            'created_at' => new DateTime(),
             ],
             [
-            'title' => 'Mori',
+            'title' => 'お気に入り！',
             'body' => 'ロングコート',
             'user_id' => '4',
-            'image_url' =>'https://res.cloudinary.com/djyg6jqy2/image/upload/v1675156827/%E3%83%AD%E3%83%B3%E3%82%B0%E3%82%B3%E3%83%BC%E3%83%88_b25ylp.jpg'
+            'image_url' =>'https://res.cloudinary.com/djyg6jqy2/image/upload/v1676111440/%E3%82%B3%E3%83%BC%E3%83%87%EF%BC%94_hkc4ne.jpg',
+            'created_at' => new DateTime(),
             ],
             [
-            'title' => 'Koro',
-            'body' => 'ロングコート',
+            'title' => '黒シャツ',
+            'body' => '普段着です。',
             'user_id' => '1',
-            'image_url' =>'https://res.cloudinary.com/djyg6jqy2/image/upload/v1675156801/lev_code2_ao8fo7.jpg'
+            'image_url' =>'https://res.cloudinary.com/djyg6jqy2/image/upload/v1676111609/%E3%82%B3%E3%83%BC%E3%83%87%EF%BC%97_gvhpn0.jpg',
+            'created_at' => new DateTime(),
             ],
             [
-            'title' => 'こんにちは',
-            'body' => 'レザースカート',
+            'title' => 'はじめまして。',
+            'body' => 'スーツスタイル',
             'user_id' => '4',
-            'image_url' =>'https://res.cloudinary.com/djyg6jqy2/image/upload/v1675156822/%E3%83%AC%E3%82%B6%E3%83%BC%E3%82%B9%E3%82%AB%E3%83%BC%E3%83%88_nkow2v.jpg'
+            'image_url' =>'https://res.cloudinary.com/djyg6jqy2/image/upload/v1676111613/%E3%82%B3%E3%83%BC%E3%83%87%EF%BC%98_rjt2sp.jpg',
+            'created_at' => new DateTime(),
             ],
      ]);
     }
